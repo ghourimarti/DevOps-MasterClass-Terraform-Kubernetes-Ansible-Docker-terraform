e13e22783d807d137fc4caed20fa633decc6d0fb
Zaini <ghourimarti@gmail.com>
2026-03-28 19:05:01 +0500

    v1 ...

--- learn/Section 56 Start with Terraform Basics/createInstance.tf
+++ learn/Section 56 Start with Terraform Basics/createInstance.tf
@@ -11,14 +11,30 @@
 
 # Configure the AWS Provider
 
-provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = var.aws_region
-}
+# provider "aws" {
+#   access_key = var.aws_access_key
+#   secret_key = var.aws_secret_key
+#   region     = var.aws_region
+# }
 
-# 
-resource "aws_instance" "zohan-kino-hano" {
-  ami           = "ami-02633e712f6dcb86f"
+# # 
+# resource "aws_instance" "zohan-kino-hano" {
+#   ami           = "ami-02633e712f6dcb86f"
+#   instance_type = "t2.micro"
+# }
+
+
+############################################################
+#      Multiple instances
+############################################################
+
+resource "aws_instance" "MyFirstInstnace" {
+  count         = 3
+  ami           = "ami-05692172625678b4e"
   instance_type = "t2.micro"
+
+  tags = {
+    # Name = "demoinstnce-${count.index}"
+    Name = "Zohana"
+  }
 }
