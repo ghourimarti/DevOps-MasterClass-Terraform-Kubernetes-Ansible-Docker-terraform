28663f097eeb960fc89fd3d658b2b385867f4960
Zaini <ghourimarti@gmail.com>
2026-03-29 00:17:04 +0500

    v1 ...

--- learn/Section 57 Terraform Variables Detailed Explanation/createInstance.tf
+++ learn/Section 57 Terraform Variables Detailed Explanation/createInstance.tf
@@ -1,5 +1,6 @@
-resource "aws_instance" "MyFirstInstnac" {
-  ami           = var.ami
+
+resource "aws_instance" "MyFirstInstnace" {
+  ami           = "ami-05692172625678b4e"
   instance_type = "t2.micro"
 
   tags = {
