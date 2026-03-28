d0fa4a618e024fda1557755c6217fa830b6e5fc8
Zaini <ghourimarti@gmail.com>
2026-03-28 10:13:18 +0500

    DONE.....

---
+++ 0Sections/IaC.sh
@@ -0,0 +1,36 @@
+############################################################
+
+# https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/latest
+
+
+# 0. 
+Write IaC
+
+# 1. 
+terraform init
+
+# 2.
+terraform plan
+
+# 3. Deploy
+terraform apply
+
+# 4.
+terraform destroy
+
+############################################################
+#      S55 IaC with Terraform
+############################################################
+
+# Install Terrraform
+
+
+############################################################
+#      S56 Start with Terraform Basics
+############################################################
+
+
+
+
+
+
