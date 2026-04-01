3dfa9ad38c46dd44c82e92191e20b1ea42002518
Zaini <ghourimarti@gmail.com>
2026-04-01 18:40:48 +0500

    v1 ...

--- learn/Section 58 Terraform Concepts Building Blocks/2-DataSources in Terraform/securitygroup.tf
+++ learn/Section 58 Terraform Concepts Building Blocks/2-DataSources in Terraform/securitygroup.tf
@@ -35,7 +35,7 @@ resource "aws_security_group" "sg-custom_us_east" {
 #               Resource 3 output
 ######################################################
 
-output "<------------------ all_cidr_blocks ---------------->" {
+output "<------------------all_cidr_blocks---------------->" {
 #   value = data.aws_ip_ranges.us_east_ip_range.cidr_blocks
   value = data.aws_ip_ranges.us_east_ip_range
 }
