831348dab4412ece909d0cf6f87e29a33cf1c7dc
Zaini <ghourimarti@gmail.com>
2026-04-02 06:59:21 +0500

    v1 ...

--- learn/Section 58 Terraform Concepts Building Blocks/4-Remote State in TF/createInstance1.tf
+++ learn/Section 58 Terraform Concepts Building Blocks/4-Remote State in TF/createInstance1.tf
@@ -33,7 +33,7 @@ data "aws_ami" "latest-ubuntu" {
 
 resource "aws_instance" "MyFirstInstnace" {
   ami           = data.aws_ami.latest-ubuntu.id # lookup(var.AMIS, var.aws_region) 
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   availability_zone = data.aws_availability_zones.avilable.names[0]
 
   tags = {
