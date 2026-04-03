1978ecfc8333ea589d61f3c8ba7b62ec15453dd6
Zaini <ghourimarti@gmail.com>
2026-04-04 01:52:47 +0500

    v1 ...

--- learn/Section 59 Terraform for AWS Cloud/1-VPC/createInstance.tf
+++ learn/Section 59 Terraform for AWS Cloud/1-VPC/createInstance.tf
@@ -3,10 +3,10 @@
 #                 1. Create aws_key_pair
 #################################################
 
-# resource "aws_key_pair" "levelup_key" {
-#     key_name = "levelup_key"
-#     public_key = file(var.PATH_TO_PUBLIC_KEY)
-# }
+resource "aws_key_pair" "levelup_key" {
+    key_name = "levelup_key"
+    public_key = file(var.PATH_TO_PUBLIC_KEY)
+}
 
 
 
@@ -17,7 +17,7 @@
 resource "aws_instance" "Instnace-public-1" {
   ami           = lookup(var.AMIS, var.aws_region)
   instance_type = "t2.micro"
-  # key_name      = aws_key_p   air.levelup_key.key_name
+  key_name      = aws_key_p   air.levelup_key.key_name
 
   vpc_security_group_ids = [aws_security_group.allow-levelup-ssh.id]
   subnet_id = aws_subnet.levelupvpc-public-1.id
