061b45d9a62331fe46a0934f613305135f4acc8d
Zaini <ghourimarti@gmail.com>
2026-04-06 01:46:15 +0500

    v1 ...

--- learn/Section 61 Terraform Modules Code ReUseability/1-Module1/main.tf
+++ learn/Section 61 Terraform Modules Code ReUseability/1-Module1/main.tf
@@ -2,7 +2,7 @@ module "ec2_cluster" {
     source = "github.com/terraform-aws-modules/terraform-aws-ec2-instance.git"
 
     name    = "my-cluster"
-    ami     = lookup(var.AMIS, var.aws_region) # "ami-0f40c8f97004632f9"
+    ami     = lookup(var.AMIS, var.aws_region) #"ami-0f40c8f97004632f9"
     instance_type          = "t2.micro"
     subnet_id   = aws_subnet.levelupvpc-private-3.id # "subnet-e92f9cc8"
 
---
+++ learn/Section 61 Terraform Modules Code ReUseability/1-Module2/variables.tf
@@ -0,0 +1,92 @@
+variable "aws_access_key" {
+  type      = string
+  sensitive = true
+  default = ""
+}
+
+variable "aws_secret_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "aws_region" {
+  type = string
+  default = "us-east-1"
+}
+
+variable "AMIS" {
+    type = map
+    default = {
+        us-east-1 = "ami-02633e712f6dcb86f"
+        us-east-2 = "ami-05692172625678b4e"
+        us-west-2 = "ami-0352d5a37fb4f603f"
+        us-west-1 = "ami-0f40c8f97004632f9"
+    }
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "levelup_key"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "levelup_key.pub"
+}
+
+variable "INSTANCE_USERNAME" {
+  default = "ubuntu"
+}
+
+
+variable "vpcname" {
+  description = "Name to be used on all the resources as identifier"
+  type        = string
+  default     = ""
+}
+
+variable "cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
+variable "instance_tenancy" {
+  description = "A tenancy option for instances launched into the VPC"
+  type        = string
+  default     = "default"
+}
+
+variable "enable_dns_hostnames" {
+  description = "Should be true to enable DNS hostnames in the VPC"
+  type        = bool
+  default     = false
+}
+
+variable "enable_dns_support" {
+  description = "Should be true to enable DNS support in the VPC"
+  type        = bool
+  default     = true
+}
+
+variable "enable_classiclink" {
+  description = "Should be true to enable ClassicLink for the VPC. Only valid in regions and accounts that support EC2 Classic."
+  type        = bool
+  default     = null
+}
+
+variable "enable_classiclink_dns_support" {
+  description = "Should be true to enable ClassicLink DNS Support for the VPC. Only valid in regions and accounts that support EC2 Classic."
+  type        = bool
+  default     = null
+}
+
+variable "enable_ipv6" {
+  description = "Requests an Amazon-provided IPv6 CIDR block."
+  type        = bool
+  default     = false
+}
+
+variable "vpcenvironment" {
+  description = "AWS VPC Environment Name"
+  type        = string
+  default     = "Development"
+}
