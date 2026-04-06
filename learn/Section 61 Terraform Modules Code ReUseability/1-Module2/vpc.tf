c2f2789957f5d66efc1a1a6d3ec3bc5c5d2b1068
Zaini <ghourimarti@gmail.com>
2026-04-06 10:15:30 +0500

    v1 ...

--- learn/Section 61 Terraform Modules Code ReUseability/1-Module2/vpc.tf
+++ learn/Section 61 Terraform Modules Code ReUseability/1-Module2/vpc.tf
@@ -9,8 +9,8 @@ resource "aws_vpc" "aws_vpc_levelup" {
   instance_tenancy                 = var.instance_tenancy
   enable_dns_hostnames             = var.enable_dns_hostnames
   enable_dns_support               = var.enable_dns_support
-  enable_classiclink               = var.enable_classiclink
-  enable_classiclink_dns_support   = var.enable_classiclink_dns_support
+  # enable_classiclink               = var.enable_classiclink
+  # enable_classiclink_dns_support   = var.enable_classiclink_dns_support
   assign_generated_ipv6_cidr_block = var.enable_ipv6
 
   tags = {
