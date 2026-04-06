5a281e8616b716cf369edf5d01bd55cfbacbd4fd
Zaini <ghourimarti@gmail.com>
2026-04-06 10:19:07 +0500

    v1 ...

--- learn/Section 61 Terraform Modules Code ReUseability/1-Module2/Development/main.tf
+++ learn/Section 61 Terraform Modules Code ReUseability/1-Module2/Development/main.tf
@@ -2,10 +2,10 @@ module "dev-vpc" {
     source                          = "../../1-Module2" #  "../../custom_vpc"
     aws_secret_key                  = var.aws_secret_key
     vpcname                         = "dev01-vpc"
-    cidr                            = "10.0.2.0/24"
-    enable_dns_support              = "true"
-    enable_classiclink              = "false"
-    enable_classiclink_dns_support  = "false"
+    cidr                            = "10.0.0.0/16"
+    enable_dns_support              = true
+    # enable_classiclink              = "false"
+    # enable_classiclink_dns_support  = "false"
     enable_ipv6                     = "true"
     vpcenvironment                  = "Development-Engineering"
 }
