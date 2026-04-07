ba5842d3eea68e65c5e88083807c2e07206cbb59
Zaini <ghourimarti@gmail.com>
2026-04-08 01:15:36 +0500

    v1 ...

---
+++ learn/Section 62 Conditions Loops in Terraform/2-functiuon-loops/terr.sh
@@ -0,0 +1,22 @@
+base64encode(string)
+base64decode(string)
+chomp(string)
+chop(string)
+chunklist(list,size)
+coalesce(string1,string2,...)
+coalescelist(list)
+compact(list)
+concat(list)
+contains(list,element)
+element(list,index)
+file(path)
+length(list)
+lookup(map,key,[default])
+timestamp()
+trimspace(string)
+# ➤ uuid() 
+
+# loops
+Count
+For
+For_each
