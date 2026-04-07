412296a04f1e43a15573b2821af84775edc8f9bb
Zaini <ghourimarti@gmail.com>
2026-04-07 16:13:42 +0500

    v1 ...

--- learn/Section 61 Terraform Modules Code ReUseability/project/environments/development/variables.tf
+++ learn/Section 61 Terraform Modules Code ReUseability/project/environments/development/variables.tf
@@ -13,3 +13,13 @@ variable "aws_region" {
   type = string
   default = "us-east-1"
 }
+
+variable "instance_ami" {
+  type    = string
+  default = "ami-02633e712f6dcb86f"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
