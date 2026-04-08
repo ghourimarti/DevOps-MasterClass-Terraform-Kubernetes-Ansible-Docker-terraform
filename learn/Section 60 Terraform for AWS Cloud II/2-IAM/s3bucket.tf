14dece0d33f17e6706a349972590f865628736c1
Zaini <ghourimarti@gmail.com>
2026-04-09 00:53:45 +0500

    v1 ...

--- learn/Section 60 Terraform for AWS Cloud II/2-IAM/s3bucket.tf
+++ learn/Section 60 Terraform for AWS Cloud II/2-IAM/s3bucket.tf
@@ -4,7 +4,7 @@
 # Create AWS S3 Bucket
 
 resource "aws_s3_bucket" "levelup-s3bucket" {
-  bucket = "levelup-bucket-141"
+  bucket = "zohee-levelup-bucket141"
   #  aws_s3_bucket_acl    = "private"  # acl
 
   tags = {
