b2f2ecc4a2b21750d385d083fb8abdf94dcec6f8
Zaini <ghourimarti@gmail.com>
2026-04-10 16:01:53 +0500

    v1 ...

--- learn/Section 60 Terraform for AWS Cloud II/4-load-balance/variables.tf
+++ learn/Section 60 Terraform for AWS Cloud II/4-load-balance/variables.tf
@@ -1,18 +1,28 @@
-variable "AWS_ACCESS_KEY" {
-    type = string
-    default = "AKIASMSIZOF42P2VUDSZ"
+variable "aws_access_key" {
+  type      = string
+  sensitive = true
+  default = ""
 }
 
-variable "AWS_SECRET_KEY" {}
+variable "aws_secret_key" {
+  type      = string
+  sensitive = true
+}
 
-variable "AWS_REGION" {
-default = "us-east-2"
+variable "aws_region" {
+  type = string
+  default = "us-east-1"
+}
+
+variable "Security_Group"{
+    type = list
+    default = ["sg-24076", "sg-90890", "sg-456789"]
 }
 
 variable "AMIS" {
     type = map
     default = {
-        us-east-1 = "ami-0f40c8f97004632f9"
+        us-east-1 = "ami-02633e712f6dcb86f"
         us-east-2 = "ami-05692172625678b4e"
         us-west-2 = "ami-0352d5a37fb4f603f"
         us-west-1 = "ami-0f40c8f97004632f9"
