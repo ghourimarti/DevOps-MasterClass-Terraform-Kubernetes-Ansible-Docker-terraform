87af330c6903019adcb6372b8e26210b459986b4
Zaini <ghourimarti@gmail.com>
2026-04-13 10:29:24 +0500

    v1 ...

--- learn/Section 60 Terraform for AWS Cloud II/3-Autoscaling/autoscaling.tf
+++ learn/Section 60 Terraform for AWS Cloud II/3-Autoscaling/autoscaling.tf
@@ -96,7 +96,7 @@ resource "aws_autoscaling_policy" "levelup-cpu-policy" {
   autoscaling_group_name = aws_autoscaling_group.levelup-autoscaling.name
   adjustment_type        = "ChangeInCapacity"
   scaling_adjustment     = "1"
-  cooldown               = "200"
+  cooldown               = "20"
   policy_type            = "SimpleScaling"
 }
 
@@ -108,10 +108,10 @@ resource "aws_cloudwatch_metric_alarm" "levelup-cpu-alarm" {
   alarm_name          = "levelup-cpu-alarm"
   alarm_description   = "Alarm once CPU Uses Increase"
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "2"
+  evaluation_periods  = "1"
   metric_name         = "CPUUtilization"
   namespace           = "AWS/EC2"
-  period              = "120"
+  period              = "20"
   statistic           = "Average"
   threshold           = "30"
 
@@ -132,7 +132,7 @@ resource "aws_autoscaling_policy" "levelup-cpu-policy-scaledown" {
   autoscaling_group_name = aws_autoscaling_group.levelup-autoscaling.name
   adjustment_type        = "ChangeInCapacity"
   scaling_adjustment     = "-1"
-  cooldown               = "200"
+  cooldown               = "20"
   policy_type            = "SimpleScaling"
 }
 
@@ -144,10 +144,10 @@ resource "aws_cloudwatch_metric_alarm" "levelup-cpu-alarm-scaledown" {
   alarm_name          = "levelup-cpu-alarm-scaledown"
   alarm_description   = "Alarm once CPU Uses Decrease"
   comparison_operator = "LessThanOrEqualToThreshold"
-  evaluation_periods  = "2"
+  evaluation_periods  = "1"
   metric_name         = "CPUUtilization"
   namespace           = "AWS/EC2"
-  period              = "120"
+  period              = "20"
   statistic           = "Average"
   threshold           = "10"
 
