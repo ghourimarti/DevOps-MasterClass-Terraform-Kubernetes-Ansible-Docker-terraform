1eccdd1fa0d9d19f1f431b9c8ee422eb83390fca
Zaini <ghourimarti@gmail.com>
2026-04-14 19:29:08 +0500

    v1 ...

--- learn/Section 63 Packer and terraform/Deploy_Custom_Image/custom_ami_deployment/createInstance.tf
+++ learn/Section 63 Packer and terraform/Deploy_Custom_Image/custom_ami_deployment/createInstance.tf
@@ -18,10 +18,17 @@ module "develop-vpc" {
 #  1. Create AWS VPC
 #################################################
 #Resource key pair
+# resource "aws_key_pair" "levelup_key" {
+#   key_name      = "levelup_key"
+#   public_key    = file(var.levelup_key) # file(var.public_key_path)
+# }
+
+
 resource "aws_key_pair" "levelup_key" {
-  key_name      = "levelup_key"
-  public_key    = file(var.public_key_path)
+    key_name = "levelup_key"
+    public_key = file(var.PATH_TO_PUBLIC_KEY)
 }
+
 
 #################################################
 #  1. Create AWS VPC
--- learn/Section 63 Packer and terraform/Deploy_Custom_Image/custom_ami_deployment/varaibles.tf
+++ learn/Section 63 Packer and terraform/Deploy_Custom_Image/custom_ami_deployment/varaibles.tf
@@ -37,3 +37,12 @@ variable "aws_region" {
   type = string
   default = "us-east-1"
 }
+
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "levelup_key"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "levelup_key.pub"
+}
