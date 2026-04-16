de02ed02830c4019cc754f1fd5669c95806bd01b
Zaini <ghourimarti@gmail.com>
2026-04-16 09:44:25 +0500

    v1 ...

--- learn/Section 64 Job Scenario 1  End to End Web-Applicatio/casestudy#1/varaible.tf
+++ learn/Section 64 Job Scenario 1  End to End Web-Applicatio/casestudy#1/varaible.tf
@@ -21,3 +21,22 @@ default = "us-east-2"
 variable "INSTANCE_TYPE" {
   default = "t2.micro"
 }
+
+
+
+
+variable "aws_access_key" {
+  type      = string
+  sensitive = true
+  default = ""
+}
+
+variable "aws_secret_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "aws_region" {
+  type = string
+  default = "us-east-1"
+}
