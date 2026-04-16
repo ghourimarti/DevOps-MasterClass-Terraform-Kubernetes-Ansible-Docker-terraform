e4e9e3e01a47f19f1e876bee939d9994367d7802
Zaini <ghourimarti@gmail.com>
2026-04-16 21:58:41 +0500

    v1 ...

--- learn/Section 64 Job Scenario 1  End to End Web-Applicatio/casestudy#1/webserver/varaible.tf
+++ learn/Section 64 Job Scenario 1  End to End Web-Applicatio/casestudy#1/webserver/varaible.tf
@@ -10,10 +10,10 @@ variable "INSTANCE_TYPE" {
 variable "AMIS" {
     type = map
     default = {
-        us-east-1 = "ami-0f40c8f97004632f9"
+        us-east-1 = "ami-02633e712f6dcb86f"
         us-east-2 = "ami-05692172625678b4e"
-        us-west-2 = "ami-02c8896b265d8c480"
-        eu-west-1 = "ami-0cdd3aca00188622e"
+        us-west-2 = "ami-0352d5a37fb4f603f"
+        us-west-1 = "ami-0f40c8f97004632f9"
     }
 }
 
